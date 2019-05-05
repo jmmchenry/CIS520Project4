296026fd1fc2e5a07a1102029aeb662f352cf5d5
Victor Ramos <vmramosa@viper.cs.ksu.edu>
2019-05-05 17:07:05 -0500

    Added Pthreads

---
+++ pthreads-4G-RAM/LCS_pthreads.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+fname='/homes/dan/625/wiki_dump.txt'
+#problem='10000 100000 500000 1000000'
+
+echo$1
+#for i in $problem
+#do
+	echo "---PROBLEM SIZE: $2---"
+	echo "---THREAD COUNT: $3---"
+	./LCS_pthreads $fname $2 $3
+#done
